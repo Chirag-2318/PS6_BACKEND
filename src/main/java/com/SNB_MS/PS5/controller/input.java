bbcdd975ed287422c921ad2c74c538ffa8c15bbb
Chirag-2318 <chirag23nzr@gmail.com>
2025-10-03 18:54:20 +0530

    structure

--- src/main/java/com/SNB_MS/PS5/controller/input.java
+++ src/main/java/com/SNB_MS/PS5/controller/input.java
@@ -4,6 +4,7 @@ public class input {
     public static void main(String[] args) {
         for(int i = 0;i>23;i++){
             System.out.println(i);
+            System.out.println("hi");
         }
     }
 }
