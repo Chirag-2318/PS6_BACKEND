922520c3158816e88001fc9579ec22b8a201010b
Chirag-2318 <chirag23nzr@gmail.com>
2025-10-03 18:52:07 +0530

    structure

---
+++ src/main/java/com/SNB_MS/PS5/controller/input.java
@@ -0,0 +1,10 @@
+package com.SNB_MS.PS5.controller;
+
+public class input {
+    public static void main(String[] args) {
+        for(int i = 0;i>23;i++){
+            System.out.println(i);
+        }
+    }
+}
+
